5cfeb69c119e682a11ec3ec8d7cd23b15eceb25b
tsegaye-24 <tsegayealamirew2016@gmail.com>
2026-02-17 20:11:50 +0300

    Remove commented code and keep final minimal fix

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -8,42 +8,6 @@ export class HttpClient {
     this.oauth2Token = new OAuth2Token("fresh-token", 10 ** 10);
   }
 
-  // request(
-  //   method: string,
-  //   path: string,
-  //   opts?: { api?: boolean; headers?: Record<string, string> }
-  // ): { method: string; path: string; headers: Record<string, string> } {
-  //   const api = opts?.api ?? false;
-  //   const headers = opts?.headers ?? {};
-
-  //   // if (api) {
-  //   //   // BUG: truthiness + instanceof check misses the "plain object" token case.
-  //   //   if (
-  //   //     !this.oauth2Token ||
-  //   //     (this.oauth2Token instanceof OAuth2Token && this.oauth2Token.expired)
-  //   //   ) {
-  //   //     this.refreshOAuth2();
-  //   //   }
-
-  //   //   if (this.oauth2Token instanceof OAuth2Token) {
-  //   //     headers["Authorization"] = this.oauth2Token.asHeader();
-  //   //   }
-  //   // }
-  //   if (api) {
-  //       if (
-  //         !this.oauth2Token ||
-  //         (this.oauth2Token instanceof OAuth2Token && this.oauth2Token.expired) ||
-  //         !(this.oauth2Token instanceof OAuth2Token)
-  //       ) {
-  //         this.refreshOAuth2();
-  //       }
-
-  //       // After refresh, it is guaranteed to be OAuth2Token
-  //       headers["Authorization"] = (this.oauth2Token as OAuth2Token).asHeader();
-  //     }
-
-  //   return { method, path, headers };
-  // }
       request(
       method: string,
       path: string,
